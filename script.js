76fb5748f9ae0f8b99fc840afd7e313e9926ca54
Charles <53397655+charlesncn@users.noreply.github.com>
2021-02-01 14:06:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 30){
+            $('.navBar').addClass("sticky");
+        }
+        else{
+            $('.navBar').removeClass("sticky");
+        }
+    });
+    // menu icon
+    $('.menu_btn').click(function(){
+        $('.navBar .menu').toggleClass("active");
+        $('.menu_btn i').toggleClass("active");
+    });
+});
