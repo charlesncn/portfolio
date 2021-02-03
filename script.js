4f51d97c9f44a487568e814685cc2f113d5afb17
Charles <53397655+charlesncn@users.noreply.github.com>
2021-02-03 08:20:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,4 +12,26 @@ $(document).ready(function(){
         $('.navBar .menu').toggleClass("active");
         $('.menu_btn i').toggleClass("active");
     });
+
+    // carousel
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
